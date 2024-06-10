ce96e9f31ce7101a7f78562a6dc60c6e341ef69d
AMOMO <731523802@qq.com>
2024-06-10 09:29:23 +0800

    添加了注释

--- MWinFormsCore/BrowserUserControl.cs
+++ MWinFormsCore/BrowserUserControl.cs
@@ -1,12 +1,13 @@
 ﻿using CefSharp.WinForms;
-
 using MWinFormsCore.CustomCef;
+using System.Windows.Forms;
 
 namespace MWinFormsCore
 {
     public partial class BrowserUserControl : UserControl
     {
         public CustomWebBrowser CefWebBrowser;
+
         public BrowserUserControl()
         {
             InitializeComponent();
@@ -15,37 +16,56 @@ namespace MWinFormsCore
 
         private void InitData()
         {
+            // 初始化自定义浏览器对象
             CefWebBrowser = new CustomWebBrowser();
+            // 设置浏览器控件填充布局
             CefWebBrowser.Dock = DockStyle.Fill;
+            // 将浏览器控件添加到 SplitContainer 的 Panel1
             browserSplitContainer.Panel1.Controls.Add(CefWebBrowser);
         }
-        public void ShowDevToolsDocked()
+
+        
+        public void ShowDevToolsDocked()// 显示开发者工具并停靠在面板中
         {
             this.Invoke(() =>
             {
-                if (browserSplitContainer.Panel2Collapsed)
+               
+                if (browserSplitContainer.Panel2Collapsed) // 如果 Panel2 被折叠，则展开
                 {
                     browserSplitContainer.Panel2Collapsed = false;
                 }
-                Control devToolsControl = GetDevToolsControl();
-                if (devToolsControl == null || devToolsControl.IsDisposed)
+
+              
+                Control devToolsControl = GetDevToolsControl();  // 获取开发者工具控件
+
+               
+                if (devToolsControl == null || devToolsControl.IsDisposed) // 如果开发者工具控件为空或已释放，则创建并显示新的开发者工具
                 {
                     CefWebBrowser.ShowDevToolsDocked(browserSplitContainer.Panel2, nameof(devToolsControl), DockStyle.Fill);
                 }
             });
         }
-        public void CloseDevToolsDocked()
+
+        
+        public void CloseDevToolsDocked()// 关闭停靠的开发者工具
         {
-            Control devToolsControl = GetDevToolsControl();
-            browserSplitContainer.Panel2.Controls.Remove(devToolsControl);
+           
+            Control devToolsControl = GetDevToolsControl(); // 获取开发者工具控件
+
+            browserSplitContainer.Panel2.Controls.Remove(devToolsControl);  // 从 Panel2 中移除开发者工具控件并释放资源
             devToolsControl?.Dispose();
-            if (!browserSplitContainer.Panel2Collapsed)
+
+           
+            if (!browserSplitContainer.Panel2Collapsed) // 如果 Panel2 未折叠，则将其折叠
             {
                 browserSplitContainer.Panel2Collapsed = true;
             }
         }
-        private Control GetDevToolsControl()
+
+       
+        private Control GetDevToolsControl() // 获取开发者工具控件
         {
+            // 查找名为 devToolsControl 的控件并返回
             Control devToolsControl = browserSplitContainer.Panel2.Controls.Find(nameof(devToolsControl), false).FirstOrDefault();
             return devToolsControl;
         }
